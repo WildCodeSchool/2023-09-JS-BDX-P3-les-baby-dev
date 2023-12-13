2c0f06bd42c82b8a1e408f09b7addfaca7f9850a
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-12-13 16:56:24 +0100

    HomePro images + responsive (missing texts)

--- frontend/src/pages/HomePro.jsx
+++ frontend/src/pages/HomePro.jsx
@@ -1,7 +1,44 @@
+import "./HomePro.css";
+
 function HomePro() {
   return (
-    <div>
-      <p>test</p>
+    <div className="container">
+      <img
+        className="visuel-simple"
+        src="./src/assets/proSimple.svg"
+        alt="Visuel Simple"
+      />
+      <h1 className="text">
+        Equipez vous du logiciel de gestion de place d’accueil de jeunes enfants
+        le plus complet
+      </h1>
+      <h4 className="info-text">
+        Cliquez sur l'une de nos solutions pour en savoir plus
+      </h4>
+      <div className="grid">
+        <img
+          className="visuel"
+          src="./src/assets/ProAgenda.svg"
+          alt="Visuel Agenda"
+        />
+        <img
+          className="visuel"
+          src="./src/assets/proAlerte.svg"
+          alt="Visuel Alerte"
+        />
+      </div>
+      <div className="grid">
+        <img
+          className="visuel"
+          src="./src/assets/ProCom.svg"
+          alt="Visuel Agenda"
+        />
+        <img
+          className="visuel"
+          src="./src/assets/proMarketing.svg"
+          alt="Visuel Alerte"
+        />
+      </div>
     </div>
   );
 }
