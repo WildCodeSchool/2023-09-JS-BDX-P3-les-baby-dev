6b38473769fa8ed3894895f1f13168988a27879a
Mathieu Chauveau <chauveau.math@gmail.com>
2024-01-10 17:11:36 +0100

    ajout route profil>inscription

--- frontend/src/pages/pages.parents/Profil.jsx
+++ frontend/src/pages/pages.parents/Profil.jsx
@@ -17,7 +17,9 @@ function Profil() {
       </div>
       <div className="list_options">
         <ul>
-          <li>Dossier d'inscription</li>
+          <Link to="/profil/inscription">
+            <li>Dossier d'inscription</li>
+          </Link>
           <Link to="/profil/myresa">
             <li>Mes réservations</li>
           </Link>
