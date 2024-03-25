da0891834ba69096f277c1c5ca831588a5e8ccde
Mathieu Chauveau <chauveau.math@gmail.com>
2024-03-25 11:15:14 +0100

    change authmiddleware

--- backend/src/middlewares/Security/auth.middleware.js
+++ backend/src/middlewares/Security/auth.middleware.js
@@ -3,7 +3,9 @@ const models = require("../../models");
 
 const authMiddleware = (req, res, next) => {
   if (!req.headers.authorization) {
-    return res.status(401).json({ error: "La racaille, ça dégage" });
+    return res
+      .status(401)
+      .json({ error: "Vous n'avez pas l'autorisation requise" });
   }
 
   return jwt.verify(
@@ -15,7 +17,7 @@ const authMiddleware = (req, res, next) => {
       }
       models.user.getProfile(data.id).then(([rows]) => {
         if (!rows.length) {
-          return res.status(401).json({ error: "T'es mort poto" });
+          return res.status(401).json({ error: "Token non admis" });
         }
         // eslint-disable-next-line prefer-destructuring
         req.user = rows[0];
